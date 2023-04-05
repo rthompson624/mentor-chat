d4d2638e604ea1ab9bb943c0327f021d2f6d8b63
Richard Thompson <rthompson624@gmail.com>
2023-04-05 14:26:12 -0400

    Added new mentors

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -2,10 +2,13 @@ import { IMentor } from "./interfaces";
 
 export const MENTORS: IMentor[] = [
   { name: 'Arnold Schwarzenegger', imageUrl: 'arnold-schwarzenegger.jpg' },
-  { name: 'Jordan Peterson', imageUrl: 'jordan-peterson.jpg' },
+  { name: 'Sarah Silverman', imageUrl: 'sarah-silverman.jpeg' },
   { name: 'Jerry Seinfeld', imageUrl: 'jerry-seinfeld.jpg' },
-  { name: 'Alan Alda', imageUrl: 'alan-alda.jpg' },
-  { name: 'Buddha', imageUrl: 'buddha.jpg' },
+  { name: 'Oprah Winfrey', imageUrl: 'oprah-winfrey.jpeg' },
+  { name: 'Donald Trump', imageUrl: 'donald-trump.jpeg' },
+  { name: 'Tina Fey', imageUrl: 'tina-fey.jpeg' },
+  { name: 'Tracy Morgan', imageUrl: 'tracy-morgan.png' },
+  { name: 'Steve Jobs', imageUrl: 'steve-jobs.png' },
 ];
 
 export const MENTOR_CHAT_API_URL: string = import.meta.env.VITE_MENTOR_CHAT_API_URL;
